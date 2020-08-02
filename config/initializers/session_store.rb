97f30c6886da2a9d71b974f986af527a1b8b35ab
jessegumtow <gumtow@gmail.com>
2020-08-02 12:11:02 -0500

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fosterconnect-client.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fosterconnect-client.herokuapp.com/registrations"
 else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fosterconnect-client.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://fosterconnect-client.herokuapp.com/registrations"
 end
