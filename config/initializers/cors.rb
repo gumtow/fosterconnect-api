66412fdf5a72d3f9b87b1c03e69584157a7aac6f
jessegumtow <gumtow@gmail.com>
2020-08-02 12:35:24 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:3000", "https://fosterconnect-client.herokuapp.com/"
+        origins "http://localhost:3000", "https://fosterconnect-client.herokuapp.com", "https://fosterconnect-api.herokuapp.com/"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
     allow do
-        origins "https://relaxed-easley-c9a18d.netlify.app/"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
-
-    allow do
-        origins "https://fosterconnect-client.herokuapp.com/"
+        origins "https://fosterconnect-client.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
