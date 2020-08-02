9b720685287ab7b974542d2ff581d757fbd584b4
jessegumtow <gumtow@gmail.com>
2020-08-02 08:23:14 -0500

    updated cors file origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origin "https://relaxed-easley-c9a18d.netlify.app/"
+        origins "https://relaxed-easley-c9a18d.netlify.app/"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
