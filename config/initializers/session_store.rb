b9b1847266ec0dc048251dc05fcc70609ba63ab3
jessegumtow <gumtow@gmail.com>
2020-08-02 22:51:04 -0500

    setting secure cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "fosterconnect-api.herokuapp.com", same_site: :lax
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "fosterconnect-api.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
