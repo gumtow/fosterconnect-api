f7d9f2dc5b8c8656f72f75158ec4cd8bb72646d8
jessegumtow <gumtow@gmail.com>
2020-08-03 20:48:34 -0500

    force ssl

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain => 'fosterconnect-api.herokuapp.com', same_site: :none, secure: :secure
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", :domain => 'fosterconnect-api.herokuapp.com', same_site: :none, secure: :secure
 
     require 'rack/utils'
 module Rack
